0d4734625be4e3644693a228a080df4caaf9c42b
Tracy Reuther <tracyreuther@gmail.com>
2019-07-05 18:44:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,7 @@ class Transfer
     if @receiver.balance > amount && @status == "complete"
       @receiver.balance -= @amount
       @sender.balance += @amount
-      @status = "revered"
+      @status = "reversed"
     end
   end
   
